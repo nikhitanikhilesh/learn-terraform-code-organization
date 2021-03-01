b2069404a3aa4856936aa6bdb5015916833b7d1e
Ubuntu <ubuntu@ip-172-31-54-64.ec2.internal>
2021-03-01 04:24:15 +0000

    added code

--- main.tf
+++ main.tf
@@ -7,9 +7,46 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "AKIAIPJ2TTMWQDBZOGQQ"
-  secret_key = "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"
+  access_key = "AKIAVECVINOBQR5BE3MU"
+  secret_key = "7BWxQhKdX3wMcxMxmC1olQxHVbRlMw5XO13Ydk8D"
   region = var.region
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "tf-example"
+  }
+}
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+resource "aws_instance" "example" {
+  ami           = "ami-03d315ad33b9d49c4"
+  instance_type = "t2.micro"
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+  tags = {
+    Name = "ExampleInstance"
+  }
 }
 
 resource "random_pet" "petname" {
